b922cff2ee6eceabfb10791f5dc18d6cd8ab37d8
admin <336891@qq.com>
2017-11-05 21:08:56 +0800

    init

---
+++ spec/toc-viewer-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import TocViewerView from '../lib/toc-viewer-view';
+
+describe('TocViewerView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
